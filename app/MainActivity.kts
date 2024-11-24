5ee8b3d20199970ad32a2d8827e0f16479f22d15
chaimaux93 <chaima.kaddouri@estiam.com>
2024-11-24 23:45:42 +0100

    Initial commit

---
+++ app/MainActivity.kts
@@ -0,0 +1,17 @@
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialiser le RecyclerView
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerViewTasks)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = TaskAdapter(listOf("Acheter des courses", "Faire du sport", "Préparer la réunion"))
+
+        // Toast de bienvenue
+        Toast.makeText(this, "Bienvenue dans l'application de gestion de tâches", Toast.LENGTH_SHORT).show()
+    }
+}
